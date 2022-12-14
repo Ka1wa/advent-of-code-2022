d7f599ec009f0da41702ce435437acff4b48388a
Jordy Ruiter <jordy@kaiwa-projects.com>
2022-12-15 00:54:07 +0900

    Fix day03 benchmarks

--- day03/benches/benchmark.rs
+++ day03/benches/benchmark.rs
@@ -1,9 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use day03::{naive};
+use day03::{part_one_naive, part_two_naive};
 
-pub fn naive_benchmark(c: &mut Criterion) {
-    c.bench_function("naive", |b| b.iter(|| naive()));
+pub fn part_one_naive_benchmark(c: &mut Criterion) {
+    c.bench_function("part_one_naive", |b| b.iter(|| part_one_naive()));
 }
 
-criterion_group!(benches, naive_benchmark);
+pub fn part_two_naive_benchmark(c: &mut Criterion) {
+    c.bench_function("part_two_naive", |b| b.iter(|| part_two_naive()));
+}
+
+criterion_group!(benches, part_one_naive_benchmark, part_two_naive_benchmark);
 criterion_main!(benches);
